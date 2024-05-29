c06e8c24aa4c4c30ac011ca1d310a7ef4ba16cc4
unknown <dartmol21@mail.ru>
2024-05-30 00:14:24 +0300

    make settings for nginx and vue 3

--- frontend/quiz-project/src/pages/QuizPage.vue
+++ frontend/quiz-project/src/pages/QuizPage.vue
@@ -6,70 +6,72 @@
 
 
           <!-- правая колонна -->
-          <div class="col"></div>
-
-
+          <!-- <div class="col"></div> -->
 
           <!-- Средняя колонна в которой и будет основной контент -->
 
           <!-- карточка с названием опроса -->
-          <q-card v-if="!state.quiz_start" class="my-card col">
-            <q-card-section>
-              <div class="q-mb-xl text-h3 text-center">Опрос</div>
-              <div class="text-h4 text-center">{{ state.quiz.quiz_title }}</div>
-              <div class="q-mt-md text-subtitle text-center">Количество вопросов: {{ state.quiz.quiz_questions_quantity }}</div>
-            </q-card-section>
-            <q-card-section>
-              <div class="column">
-                <q-btn color="primary" label="ПРОЙТИ ОПРОС" @click="state.quiz_start=true"  size="xl"/>
-              </div>
-            </q-card-section>
-          </q-card>
+          <div class="q-pa-md example-row-equal-width">
+            <div class="row justify-center">
+              <q-card v-if="!state.quiz_start" :class="[(width > 1200)  ? 'my-card col-5': (width > 900) ? 'my-card col-8': 'my-card col']">
+                <q-card-section>
+                  <div :class="[(width > 500)  ? 'q-mb-xl text-h3 text-center' : 'q-mb-md text-h5 text-center']">Опрос</div>
+                  <div :class="[(width > 500)  ? 'text-h4 text-center' : 'text-h6 text-center']">{{ state.quiz.quiz_title }}</div>
+                  <div class="q-mt-md text-subtitle text-center">Количество вопросов: {{ state.quiz.quiz_questions_quantity }}</div>
+                </q-card-section>
+                <q-card-section>
+                  <div class="column">
+                    <q-btn color="primary" label="ПРОЙТИ ОПРОС" @click="state.quiz_start=true"  size="xl"/>
+                  </div>
+                </q-card-section>
+              </q-card>
 
-          <!-- карточка с информацией о вопросе из опроса -->
-          <q-card v-if="state.quiz_start  && !state.quiz_finish" class="my-card col">
-            <!-- <img src="https://cdn.quasar.dev/img/mountains.jpg"> -->
-            <q-card-section>
-              <div class="q-mb-xl text-h6 text-center">{{ state.quiz.quiz_title }}</div>
-              <div class="text-h4 text-center">{{ state.current_question_info.q_index }}. {{ state.current_question_info.q_title }}</div>
-            </q-card-section>
-            <q-card-section>
-              <div class="column justify-center">
-                  <q-radio class="q-my-xs text-h6" v-model="state.user_choise"  v-for="(choise) in  state.current_question_info.q_choises" :key="choise.ch_id" :val="choise.ch_id">
-                  {{ choise.ch_title }}
-                </q-radio  >
-              </div>
-
-
-              <!-- кнопка следующего вопроса -->
-              <div v-if="state.user_choise !='' &&  ((state.current_question + 1) < state.quiz.quiz_questions_quantity)" class="q-mt-xl column">
-                <q-btn  size="lg" color="primary" label="следующий вопрос" @click="nextQestion()" />
-              </div>
-
-              <div v-if="state.user_choise !='' && state.current_question + 1 ==  state.quiz.quiz_questions_quantity" class="q-mt-xl column">
-                <q-btn size="lg" color="primary" label="Отправить резульатты" @click="sendAnswers()" />
-              </div>
-            
-            </q-card-section>
-          </q-card>
+              <!-- карточка с информацией о вопросе из опроса -->
+              <q-card v-if="state.quiz_start  && !state.quiz_finish" :class="[(width > 1200)  ? 'my-card col-5': (width > 900) ? 'my-card col-8': 'my-card col']">
+                <!-- <img src="https://cdn.quasar.dev/img/mountains.jpg"> -->
+                <q-card-section>
+                  <div :class="[(width > 1200) ? 'q-mb-sm text-h6 text-center': (width > 900) ? 'mid-text' : 'small-text']">{{ state.quiz.quiz_title }}</div>
+                  <div :class="[(width > 1200) ? 'text-h5 text-center' : (width > 900) ? 'mid-title' : 'small-title']"><b>{{ state.current_question_info.q_index }}. {{ state.current_question_info.q_title }}</b></div>
+                </q-card-section>
+                <q-card-section>
+                  <div class="column justify-center">
+                      <q-radio class="q-mb-xs text-h6" v-model="state.user_choise"  v-for="(choise) in  state.current_question_info.q_choises" :key="choise.ch_id" :val="choise.ch_id">
+                      <div :class="[(width > 1200) ? '': (width > 900) ? 'mid-quiz' : 'small-quiz']">{{ choise.ch_title }}</div>
+                    </q-radio  >
+                  </div>
 
 
-          <!-- карточка с информацией о том, что опрос пройден -->
-          <q-card v-if="state.quiz_finish" class="my-card">
-            <q-card-section>
-              <div class="text-h3 text-center">Спасибо за участие в опросе!</div>
-              <div class="q-pa-md text-center col">
-                <q-icon color="green" name="check" size="10em"/>
-              </div>
-            </q-card-section>
-            <!-- <q-card-section>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
-            </q-card-section> -->
-          </q-card>
+                  <!-- кнопка следующего вопроса -->
+                  <div v-if="state.user_choise !='' &&  ((state.current_question + 1) < state.quiz.quiz_questions_quantity)" class="q-mt-xs column">
+                    <q-btn  size="lg" color="primary" label="следующий вопрос" @click="nextQestion()" />
+                  </div>
+
+                  <div v-if="state.user_choise !='' && state.current_question + 1 ==  state.quiz.quiz_questions_quantity" class="q-mt-xl column">
+                    <q-btn size="lg" color="primary" label="Отправить резульатты" @click="sendAnswers()" />
+                  </div>
+                
+                </q-card-section>
+              </q-card>
+
+
+              <!-- карточка с информацией о том, что опрос пройден -->
+              <q-card v-if="state.quiz_finish" :class="[(width > 1200)  ? 'my-card col-5': (width > 900) ? 'my-card col-8': 'my-card col']">
+                <q-card-section>
+                  <div class="text-h3 text-center">Спасибо за участие в опросе!</div>
+                  <div class="q-pa-md text-center col">
+                    <q-icon color="green" name="check" size="10em"/>
+                  </div>
+                </q-card-section>
+                <!-- <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                </q-card-section> -->
+              </q-card>
+            </div>
+          </div>
 
           
           <!-- левая колонна -->
-          <div class="col"></div>
+          <!-- <div class="col"></div> -->
 
 
     </q-page>
@@ -79,10 +81,13 @@
 <script setup>
 import axios from 'axios';
 import { ref, onMounted, reactive } from 'vue';
+import { useWindowSize  } from '@vueuse/core';
+
+const { width, height } = useWindowSize()
 
 //состояние страницы
 const state = reactive({
-  current_question : 164,
+  current_question : 0,
   quiz_start: false,
   current_question_info: {},
   quiz : {},
@@ -152,3 +157,40 @@ defineOptions({
   name: 'QuizPage'
 });
 </script>
+
+<style>
+
+
+
+.mid-title{
+  font-size: 24px;
+  text-align: center;
+}
+
+.mid-text{
+  font-size: 18px;
+  text-align: center;
+}
+
+.small-title{
+  font-size: 20px;
+  text-align: center;
+}
+
+.small-text{
+  font-size: 16px;
+  text-align: center;
+}
+
+.mid-quiz{
+  font-size: 20px;
+  text-align: center;
+}
+
+.small-quiz{
+  font-size: 14px;
+  text-align: center;
+}
+
+
+</style>
